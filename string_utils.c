7a979a2981592ba3ca3e3a3f573daa4a7657bf61
EL-HOUSS-BRAHIM <87819687+EL-HOUSS-BRAHIM@users.noreply.github.com>
2023-10-19 16:54:55 +0100

    test

--- string_utils.c
+++ string_utils.c
@@ -69,10 +69,10 @@ return (NULL);
 while (isspace((unsigned char)*str))
 str++;
 if (*str == 0)
-return str;
+return (str);
 end = str + strlen(str) - 1;
 while (end > str && isspace((unsigned char)*end))
 end--;
 end[1] = '\0';
-return str;
+return (str);
 }

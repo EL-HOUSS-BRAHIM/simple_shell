620a5421b5a5d7404b220a3b0f46f8dfcc113e7f
EL-HOUSS-BRAHIM <87819687+EL-HOUSS-BRAHIM@users.noreply.github.com>
2023-10-19 22:06:50 +0100

    test

--- setenv_unsetenv.c
+++ setenv_unsetenv.c
@@ -1,57 +1,61 @@
 #include "shell.h"
 /**
- * setenv - Set an environment variable or update its value.
- * @name: The name of the environment variable.
- * @value: The value to set or update.
- * @overwrite: Flag to indicate whether to overwrite the variable if it exists.
- * Return: 0 on success, -1 on failure.
- */
+ * setenv - Set an environment variable or update its value.
+ * @name: The name of the environment variable.
+ * @value: The value to set or update.
+ * @overwrite: Flag to indicate whether to overwrite the variable if it exists.
+ * Return: 0 on success, -1 on failure.
+ */
 int setenv(const char *name, const char *value, int overwrite)
 {
-if (name == NULL || name[0] == '\0' || (value == NULL && !overwrite))
-{
+char *env_var;
+/* Check for invalid arguments. */
+if (name == NULL || name[0] == '\0' || (value == NULL && !overwrite)) {
 fprintf(stderr, "setenv: invalid arguments\n");
 return (-1);
 }
-if (overwrite || getenv(name) == NULL)
-{
-if (putenv(strdup(name)) != 0)
-{
+/* Allocate memory for the environment variable. */
+env_var = malloc(strlen(name) + strlen(value) + 2);
+if (env_var == NULL) {
 perror("setenv");
 return (-1);
 }
-}
-char *env_var = malloc(strlen(name) + strlen(value) + 2);
-if (!env_var)
-{
-perror("setenv");
-return (-1);
-}
-strcpy(env_var, name);
-strcat(env_var, "=");
-strcat(env_var, value);
-if (putenv(env_var) != 0)
-{
+/* Check if the variable already exists and overwrite it if necessary. */
+if (overwrite || getenv(name) == NULL) {
+if (putenv(strdup(name)) != 0) {
 perror("setenv");
 free(env_var);
 return (-1);
 }
+}
+/* Construct the environment variable string. */
+strcpy(env_var, name);
+strcat(env_var, "=");
+strcat(env_var, value);
+/* Set the environment variable. */
+if (putenv(env_var) != 0) {
+perror("setenv");
+free(env_var);
+return (-1);
+}
+/* Free the allocated memory. */
+free(env_var);
 return (0);
 }
 /**
- * unsetenv - Unset an environment variable.
- * @name: The name of the environment variable to unset.
- * Return: 0 on success, -1 on failure.
- */
+ * unsetenv - Unset an environment variable.
+ * @name: The name of the environment variable to unset.
+ * Return: 0 on success, -1 on failure.
+ */
 int unsetenv(const char *name)
 {
-if (name == NULL || name[0] == '\0' || getenv(name) == NULL)
-{
+/* Check for invalid arguments. */
+if (name == NULL || name[0] == '\0' || getenv(name) == NULL) {
 fprintf(stderr, "unsetenv: invalid arguments\n");
 return (-1);
 }
-if (putenv(strdup(name)) != 0)
-{
+/* Set the environment variable to an empty string. */
+if (putenv(strdup(name)) != 0) {
 perror("unsetenv");
 return (-1);
 }
